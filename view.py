33236c5157a5b4f1e7a48b7a862c9b56efe756fd
Anson Quon <aquon@berkeley.edu>
2023-06-17 15:33:05 -0700

    sample model

--- view.py
+++ view.py
@@ -4,13 +4,24 @@ import openai
 
 app = Flask(__name__)
 
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, 
+    )
+    return response.choices[0].message["content"]
+
+
 @app.route('/')
 def home():
 
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
-    return render_template('index.html')
+    return get_completion("What is 1+1?")
 
 if __name__ == "__main__":
     #port = int(os.environ.get('PORT', 5000))
