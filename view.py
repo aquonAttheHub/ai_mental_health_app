5c14744d15ea80974830c521d65db016ea833a39
Anson Quon <aquon@berkeley.edu>
2023-06-17 16:07:01 -0700

    Updated model to a more complex one for more complex behavior

--- view.py
+++ view.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 
 
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
+def get_completion(messages, model="gpt-3.5-turbo", temperature=0, max_tokens = 500):
+    #messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0, 
+        temperature=temperature,
+        max_tokens=max_tokens
     )
     return response.choices[0].message["content"]
 
@@ -21,7 +22,54 @@ def home():
 
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    return get_completion("What is 1+1?")
+    delimiter = "####"
+    system_message = f"""
+    You will be provided with customer service queries.
+    The customer service query will be delimited with 
+    {delimiter} characters.
+    Classify each query into a primary category
+    and a secondary category. 
+    Provide your output in json format with the
+    keys: primary and secondary.
+
+    Primary categories: Billing, Technical Support,
+    Account Management, or General Inquiry.
+
+    Billing secondary categories:
+    Unsubscribe or upgrade
+    Add a payment method
+    Explanation for charge
+    Dispute a charge
+
+    Technical Support secondary categories:
+    General troubleshooting
+    Device compatibility
+    Software updates
+
+    Account Management secondary categories:
+    Password reset
+    Update personal information
+    Close account
+    Account security
+
+    General Inquiry secondary categories:
+    Product information
+    Pricing
+    Feedback
+    Speak to a human
+
+    """
+    user_message = f"""I want you to delete my profile and all of my user data"""
+    messages =  [  
+    {'role':'system', 
+    'content': system_message},    
+    {'role':'user', 
+    'content': f"{delimiter}{user_message}{delimiter}"},  
+    ]
+    
+    response = get_completion(messages)
+    print(messages)
+    return response
 
 if __name__ == "__main__":
     #port = int(os.environ.get('PORT', 5000))
