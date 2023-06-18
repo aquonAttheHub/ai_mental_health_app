13ac6ae3d620a1718b62db1785b33e5b179c7b6f
Anson Quon <aquon@berkeley.edu>
2023-06-18 00:22:18 -0700

    basic chatbot working

--- view.py
+++ view.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
@@ -17,59 +19,75 @@ def get_completion(messages, model="gpt-3.5-turbo", temperature=0, max_tokens = 
     return response.choices[0].message["content"]
 
 
+
 @app.route('/')
 def home():
+    return render_template("index.html")
 
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+@app.route("/chatbot", methods=["POST"])
+def chatbot():
+    #Get the message input from the user
+    user_input = request.form["message"]
+    #Use the OpenAI API to generate a response
+    chat_history = []
+    # messages = [ {'role':'system', 'content':"""
+    # You are OrderBot, an automated service to collect orders for a pizza restaurant. \
+    # You first greet the customer, then collects the order, \
+    # and then asks if it's a pickup or delivery. \
+    # You wait to collect the entire order, then summarize it and check for a final \
+    # time if the customer wants to add anything else. \
+    # If it's a delivery, you ask for an address. \
+    # Finally you collect the payment.\
+    # Make sure to clarify all options, extras and sizes to uniquely \
+    # identify the item from the menu.\
+    # You respond in a short, very conversational friendly style. \
+    # The menu includes \
+    # pepperoni pizza  12.95, 10.00, 7.00 \
+    # cheese pizza   10.95, 9.25, 6.50 \
+    # eggplant pizza   11.95, 9.75, 6.75 \
+    # fries 4.50, 3.50 \
+    # greek salad 7.25 \
+    # Toppings: \
+    # extra cheese 2.00, \
+    # mushrooms 1.50 \
+    # sausage 3.00 \
+    # canadian bacon 3.50 \
+    # AI sauce 1.50 \
+    # peppers 1.00 \
+    # Drinks: \
+    # coke 3.00, 2.00, 1.00 \
+    # sprite 3.00, 2.00, 1.00 \
+    # bottled water 5.00 \
+    # """} ]
 
-    delimiter = "####"
-    system_message = f"""
-    You will be provided with customer service queries.
-    The customer service query will be delimited with 
-    {delimiter} characters.
-    Classify each query into a primary category
-    and a secondary category. 
-    Provide your output in json format with the
-    keys: primary and secondary.
 
-    Primary categories: Billing, Technical Support,
-    Account Management, or General Inquiry.
+    # response = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=messages,
+    #     temperature=0.9,
+    #     max_tokens=500
+    # )
 
-    Billing secondary categories:
-    Unsubscribe or upgrade
-    Add a payment method
-    Explanation for charge
-    Dispute a charge
+    prompt = f"User: {user_input}\nChatbot: "
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0,
+        stop=["\nUser: ", "\nChatbot: "]
+    )
+    bot_response = response.choices[0].text.strip()
 
-    Technical Support secondary categories:
-    General troubleshooting
-    Device compatibility
-    Software updates
+    chat_history.append(f"User: {user_input}\nChatbot: {bot_response}")
 
-    Account Management secondary categories:
-    Password reset
-    Update personal information
-    Close account
-    Account security
 
-    General Inquiry secondary categories:
-    Product information
-    Pricing
-    Feedback
-    Speak to a human
+    #Extract the response text from the OpenAPI result
+    # bot_response = response.choices[0].message["content"]
+    # chat_history.append(f"User: {user_input}\nChatbot: {bot_response}")
+    return render_template("chatbot.html", user_input=user_input, bot_response=bot_response)
 
-    """
-    user_message = f"""I want you to delete my profile and all of my user data"""
-    messages =  [  
-    {'role':'system', 
-    'content': system_message},    
-    {'role':'user', 
-    'content': f"{delimiter}{user_message}{delimiter}"},  
-    ]
-    
-    response = get_completion(messages)
-    print(messages)
-    return response
 
 if __name__ == "__main__":
     #port = int(os.environ.get('PORT', 5000))
