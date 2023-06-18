cfd7ab552b3914b517771923bbc263a5b581fcb4
charisseliu <charisseliu@berkeley.edu>
2023-06-17 19:28:34 -0700

    ummm did stuff

--- view.py
+++ view.py
@@ -1,26 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import os
 import openai
+import json
+from openai.error import RateLimitError
 
 app = Flask(__name__)
 
 
 
-def get_completion(messages, model="gpt-3.5-turbo", temperature=0, max_tokens = 500):
-    #messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature,
-        max_tokens=max_tokens
-    )
-    return response.choices[0].message["content"]
+# def get_completion(messages, model="gpt-3.5-turbo", temperature=0, max_tokens = 500):
+#     #messages = [{"role": "user", "content": prompt}]
+#     response = openai.ChatCompletion.create(
+#         model=model,
+#         messages=messages,
+#         temperature=temperature,
+#         max_tokens=max_tokens
+#     )
+#     return response.choices[0].message["content"]
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/gpt3', methods=['GET', 'POST'])
+def gpt3():
+    user_input = request.args.get('user_input') if request.method == 'GET' else request.form['user_input']
+    messages = [{"role": "user", "content": user_input}]
+
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=0,
+            max_tokens=500
+        )
+        content = response.choices[0].message["content"]
+    except RateLimitError:
+        content = "The server is experiencing a high volume of requests. Please try again later."
+
+    return jsonify(content=content)
+
 def home():
 
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = "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"
 
     delimiter = "####"
     system_message = f"""
